be1bebb70f4d2098ec125f50955a54f123166457
David Tanzer <business@davidtanzer.net>
2018-09-07 06:42:57 +0200

    Added stub files for hangman exercise

---
+++ 02-hangman/hangman-test.py
@@ -0,0 +1,10 @@
+import unittest
+import hangman
+
+class TestHangman(unittest.TestCase):
+
+	def test_fixme_give_me_a_better_name(self):
+		self.fail("implement me")
+
+if __name__ == '__main__':
+	unittest.main()
